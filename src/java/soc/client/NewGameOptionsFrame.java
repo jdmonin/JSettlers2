1df985b48cb6e760999925da12439f7971af91bd
Jeremy D Monin <jeremy@nand.net>
2009-07-31 17:57:44 +0000

    new-game opt frame confirm min-vers, range-check and warn int opts, remove unknowns

--- src/java/soc/client/NewGameOptionsFrame.java
+++ src/java/soc/client/NewGameOptionsFrame.java
@@ -44,7 +44,9 @@ import java.awt.event.MouseListener;
 import java.awt.event.TextEvent;
 import java.awt.event.TextListener;
 import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Hashtable;
+import java.util.Map;
 
 import soc.game.SOCGameOption;
 import soc.message.SOCStatusMessage;
@@ -77,7 +79,9 @@ public class NewGameOptionsFrame extends Frame
     /** is this for display only? */
     private boolean readOnly;
 
-    /** Contains this game's {@link SOCGameOption}s, or null if none */
+    /** Contains this game's {@link SOCGameOption}s, or null if none.
+     *  Unknowns (OTYPE_UNKNOWN) are removed in initInterface_options.
+     */
     private Hashtable opts;
 
     /** Key = AWT control; value = {@link SOCGameOption}. Empty if opts is null.  */
@@ -111,6 +115,7 @@ public class NewGameOptionsFrame extends Frame
      *                 is pressed, so the next OptionsFrame will default to the values the user has chosen.
      *                 To preserve them, call {@link SOCGameOption#cloneOptions(Hashtable)} beforehand.
      *                 Null if server doesn't support game options.
+     *                 Unknown options ({@link SOCGameOption#OTYPE_UNKNOWN}) will be removed.
      * @param forPractice Will this game be on local practice server, vs remote tcp server?
      * @param readOnly    Is this display-only (for use during a game), or can it be changed?
      */
@@ -281,17 +286,25 @@ public class NewGameOptionsFrame extends Frame
             gbc.gridwidth = GridBagConstraints.REMAINDER;
             gbl.setConstraints(L, gbc);
             bp.add(L);
-            return;
+            return;  // <---- Early return: no options ----
         }
 
         // TODO sort options
 
         gbc.anchor = GridBagConstraints.WEST;
-        for (Enumeration e = opts.keys(); e.hasMoreElements(); )
+
+        // use Iterator so we can remove from the hash if unknown
+
+        for (Iterator ikv = opts.entrySet().iterator();
+            ikv.hasNext(); )
         {
-            SOCGameOption op = (SOCGameOption) opts.get(e.nextElement());
+            Map.Entry okv = (Map.Entry) ikv.next();
+            SOCGameOption op = (SOCGameOption) okv.getValue();
             if (op.optType == SOCGameOption.OTYPE_UNKNOWN)
-                continue;  // <-- Must skip this one --
+            {
+                ikv.remove();
+                continue;  // <-- Removed, Go to next entry --
+            }
 
             switch (op.optType)  // OTYPE_*
             {
@@ -508,8 +521,8 @@ public class NewGameOptionsFrame extends Frame
             Object src = ae.getSource();
             if (src == create)
             {
-                // Ask client to set up and start a practice game
-                clickCreate();
+                // Check options, ask client to set up and start a practice game
+                clickCreate(true);
                 return;
             }
 
@@ -537,7 +550,7 @@ public class NewGameOptionsFrame extends Frame
     }
 
     /** "Connect..." from connect setup; check fields, etc */
-    private void clickCreate()
+    private void clickCreate(final boolean checkOptionsMinVers)
     {
         String gmName = gameName.getText().trim();
         if (gmName.length() == 0)
@@ -557,11 +570,11 @@ public class NewGameOptionsFrame extends Frame
         }
 
         if (cl.readValidNicknameAndPassword()
-            && readOptsValuesFromControls())
+            && readOptsValuesFromControls(checkOptionsMinVers))
         {
             cl.askStartGameWithOptions(gmName, forPractice, opts);
         } else {
-            return;  // TODO print an err msg
+            return;  // TODO readvalidnick should update dia's status line; readopts does already
         }
 
         dispose();
@@ -585,16 +598,22 @@ public class NewGameOptionsFrame extends Frame
 
     /**
      * Read option values from controls, as prep to request the new game.
+     * If there is a problem (out of range, bad character in integer field, etc),
+     * set {@link #msgText} and set focus on the field.
+     * @param checkOptionsMinVers Warn the user if the options will require a
+     *           minimum client version?  Won't do so if {@link #forPractice} is set,
+     *           because this isn't a problem for local practice games.
      * @return true if all were read OK, false if a problem (such as NumberFormatException)
      */
-    private boolean readOptsValuesFromControls()
+    private boolean readOptsValuesFromControls(final boolean checkOptionsMinVers)
     {
         if (readOnly)
-            return false;  // shouldn't be called in the first place
+            return false;  // shouldn't be called in that case
+
         boolean allOK = true;
         for (Enumeration e = controlsOpts.keys(); e.hasMoreElements(); )
         {
-            Object ctrl = e.nextElement();
+            Component ctrl = (Component) e.nextElement();
             SOCGameOption op = (SOCGameOption) controlsOpts.get(ctrl);
             
             // OTYPE_* - new option types may have new AWT control objects, or
@@ -614,10 +633,21 @@ public class NewGameOptionsFrame extends Frame
                 } else {
                     try   // OTYPE_INT, OTYPE_INTBOOL
                     {
-                        op.setIntValue(Integer.parseInt(txt));
+                        int iv = Integer.parseInt(txt);
+                        op.setIntValue(iv);
+                        if (iv != op.getIntValue())
+                        {
+                            allOK = false;
+                            msgText.setText
+                                ("Out of range: Should be " + op.minIntValue
+                                 + " to " + op.maxIntValue);
+                            ctrl.requestFocusInWindow();
+                        }
                     } catch (NumberFormatException ex)
                     {
                         allOK = false;
+                        msgText.setText("Please use only digits here.");
+                        ctrl.requestFocusInWindow();
                     }
                 }
             }
@@ -633,6 +663,16 @@ public class NewGameOptionsFrame extends Frame
 
         }  // for(opts)
 
+        if (allOK && checkOptionsMinVers && ! forPractice)
+        {
+            int optsVers = SOCGameOption.optionsMinimumVersion(controlsOpts);
+            if (optsVers > -1)
+            {
+                allOK = false;
+                new VersionConfirmDialog(this, optsVers).show();
+            }
+        }
+
         return allOK;
     }
 
@@ -647,7 +687,7 @@ public class NewGameOptionsFrame extends Frame
             switch (e.getKeyCode())
             {
             case KeyEvent.VK_ENTER:
-                clickCreate();
+                clickCreate(true);
                 break;
 
             case KeyEvent.VK_CANCEL:
@@ -763,7 +803,7 @@ public class NewGameOptionsFrame extends Frame
             switch (e.getKeyCode())
             {
             case KeyEvent.VK_ENTER:
-                clickCreate();
+                clickCreate(true);
                 break;
 
             case KeyEvent.VK_CANCEL:
@@ -783,5 +823,54 @@ public class NewGameOptionsFrame extends Frame
         }
 
     }  // public inner class IntTextField
+
+
+    /**
+     * This is the dialog to ask user if these options' required
+     * minimum client version is OK.
+     *
+     * @author Jeremy D Monin <jeremy@nand.net>
+     */
+    private class VersionConfirmDialog extends AskDialog
+    {
+        /**
+         * Creates a new VersionConfirmDialog.
+         *
+         * @param cli      Player client interface
+         * @param gamePI   Current game's player interface
+         */
+        public VersionConfirmDialog(NewGameOptionsFrame ngof, int minVers)
+        {
+            super(cl, ngof, "Confirm options minimum version",
+                "Client version " + minVers + " or higher is required for these game options.\nOlder clients won't be able to join.",
+                "Create with these options", "Change options", false, true);
+        }
+
+        /**
+         * React to the Create button.
+         */
+        public void button1Chosen()
+        {
+            clickCreate(false);
+        }
+
+        /**
+         * React to the Change button.
+         */
+        public void button2Chosen()
+        {
+            dispose();
+        }
+
+        /**
+         * React to the dialog window closed by user, or Esc pressed. (same as Change button)
+         */
+        public void windowCloseChosen()
+        {
+            button2Chosen();
+        }
+
+    }  // private inner class VersionConfirmDialog
+
 
 }  // public class NewGameOptionsFrame
