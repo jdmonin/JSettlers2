2b8c6c97eac01bcd05da530a9104ea5730b4d4bb
Jeremy D Monin <jeremy@nand.net>
2009-11-28 12:41:44 +0000

    add standard proj file heading, attribution based on jsettlers, jsettlers2 cvs logs as reverted

--- src/java/soc/robot/SOCRobotDM.java
+++ src/java/soc/robot/SOCRobotDM.java
@@ -1,3 +1,24 @@
+/**
+ * Java Settlers - An online multiplayer version of the game Settlers of Catan
+ * This file copyright (C) 2003-2004  Robert S. Thomas
+ * Portions of this file copyright (C) 2009 Jeremy D Monin <jeremy@nand.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * The maintainer of this program can be reached at jsettlers@nand.net
+ **/
 package soc.robot;
 
 import java.text.DecimalFormat;
