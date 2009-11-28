939336369d5e8d456d2cff38481fe763679284c3
Jeremy D Monin <jeremy@nand.net>
2009-11-28 13:32:38 +0000

    add attribution to RST: Came from SOCRobotBrain

--- src/java/soc/robot/DiscardStrategy.java
+++ src/java/soc/robot/DiscardStrategy.java
@@ -1,6 +1,7 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * This file copyright (C) 2008 Christopher McNeil <http://sourceforge.net/users/cmcneil>
+ * Portions of this file copyright (C) 2003-2004 Robert S. Thomas
  * Portions of this file copyright (C) 2008 Eli McGowan <http://sourceforge.net/users/emcgowan>
  * Portions of this file copyright (C) 2009 Jeremy D Monin <jeremy@nand.net>
  *
