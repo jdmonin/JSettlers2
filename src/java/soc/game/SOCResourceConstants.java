17e8d4642d245bcae62e2c46d3868c6a885b7325
Jeremy D Monin <jeremy@nand.net>
2009-12-03 11:51:38 +0000

    adj (c) due to recent adds

--- src/java/soc/game/SOCResourceConstants.java
+++ src/java/soc/game/SOCResourceConstants.java
@@ -1,7 +1,7 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas
- * Some documentation javadocs here are Copyright (C) 2009 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2009 Jeremy D Monin <jeremy@nand.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
