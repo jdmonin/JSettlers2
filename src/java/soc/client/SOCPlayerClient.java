7d0cc529dbecc6a5a10a698d89653e310e141b31
Jeremy D Monin <jeremy@nand.net>
2012-10-01 21:59:33 -0400

    comments

--- src/java/soc/client/SOCPlayerClient.java
+++ src/java/soc/client/SOCPlayerClient.java
@@ -2,7 +2,7 @@
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
  * Portions of this file Copyright (C) 2007-2012 Jeremy D Monin <jeremy@nand.net>
- * Portions of this file Copyright (C) 2012 Paul Bilnoski <paul@bilnoski.net> - GameStatistics
+ * Portions of this file Copyright (C) 2012 Paul Bilnoski <paul@bilnoski.net> - GameStatistics, nested class refactoring
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -5009,8 +5009,9 @@ public class SOCPlayerClient extends Applet
                 {
                     ((Frame) parent).setTitle("JSettlers server " + Version.version()
                         + " - port " + tport);
-                } catch (Throwable t)
-                {}
+                } catch (Throwable t) {
+                    // no titlebar is fine
+                }
             }
         }
         
