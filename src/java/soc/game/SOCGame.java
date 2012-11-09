00a678f08068320506234e3b7a78a8cb9f7adf97
Jeremy D Monin <jeremy@nand.net>
2012-11-09 06:02:40 -0500

    SOCGame +putPieceCommon_checkFogHexes

--- src/java/soc/game/SOCGame.java
+++ src/java/soc/game/SOCGame.java
@@ -2067,31 +2067,8 @@ public class SOCGame implements Serializable, Cloneable
          */
         if (hasSeaBoard && isAtServer && (pp instanceof SOCRoad))
         {
-            int fogRevealedHexCoord = -1;
-
             final int[] endHexes = ((SOCBoardLarge) board).getAdjacentHexesToEdgeEnds(pp.getCoordinates());
-            for (int i = 0; i < 2; ++i)
-            {
-                final int hexCoord = endHexes[i];
-                if ((hexCoord != 0) && (board.getHexTypeFromCoord(hexCoord) == SOCBoardLarge.FOG_HEX))
-                {
-                    fogRevealedHexCoord = hexCoord;
-                    break;
-                    // No need to keep looking, because only one end of the edge is new;
-                    // player was already at the other end, so it can't be fog.
-                }
-            }
-
-            if (fogRevealedHexCoord != -1)
-            {
-                // Reveal it before placing the new piece, so it's easier for
-                // players and bots to updatePotentials (their data about the
-                // board reachable through their roads/ships).
-                ((SOCBoardLarge) board).revealFogHiddenHex(fogRevealedHexCoord);
-                if (scenarioEventListener != null)
-                    scenarioEventListener.gameEvent
-                        (this, SOCScenarioGameEvent.SGE_FOG_HEX_REVEALED, new Integer(fogRevealedHexCoord));
-            }
+            putPieceCommon_checkFogHexes(endHexes);
         }
 
         /**
@@ -2287,6 +2264,42 @@ public class SOCGame implements Serializable, Cloneable
         if (active && ! debugFreePlacement)
         {
             advanceTurnStateAfterPutPiece();
+        }
+    }
+
+    /**
+     * On the large sea board, look for and reveal any adjacent fog hex,
+     * if we're placing a road or ship touching the fog hex's corner node.
+     * Called only at server.
+     *
+     * @param hexCoords  Hex coordinates to check type for {@link SOCBoardLarge#FOG_HEX}
+     * @since 2.0.00
+     */
+    private final void putPieceCommon_checkFogHexes(final int[] hexCoords)
+    {
+        int fogRevealedHexCoord = -1;
+
+        for (int i = 0; i < 2; ++i)
+        {
+            final int hexCoord = hexCoords[i];
+            if ((hexCoord != 0) && (board.getHexTypeFromCoord(hexCoord) == SOCBoardLarge.FOG_HEX))
+            {
+                fogRevealedHexCoord = hexCoord;
+                break;
+                // No need to keep looking, because only one end of the edge is new;
+                // player was already at the other end, so it can't be fog.
+            }
+        }
+
+        if (fogRevealedHexCoord != -1)
+        {
+            // Reveal it before placing the new piece, so it's easier for
+            // players and bots to updatePotentials (their data about the
+            // board reachable through their roads/ships).
+            ((SOCBoardLarge) board).revealFogHiddenHex(fogRevealedHexCoord);
+            if (scenarioEventListener != null)
+                scenarioEventListener.gameEvent
+                    (this, SOCScenarioGameEvent.SGE_FOG_HEX_REVEALED, new Integer(fogRevealedHexCoord));
         }
     }
 
