0da87e7b84057abd1ff50a3801f6e76c12f068d4
lartkma <lartkma@gmail.com>
2013-01-04 00:13:46 -0500

    String manager created, defined strings files for client, build adds those files

---
+++ src/java/soc/util/SOCStringManager.java
@@ -0,0 +1,50 @@
+/**
+ * Java Settlers - An online multiplayer version of the game Settlers of Catan
+ * Copyright (C) 2013 Luis A. Ramirez <lartkma@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The maintainer of this program can be reached at lartkma@gmail.com 
+ **/
+package soc.util;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+/**
+ * TODO Write JavaDoc.
+ * @author lartkma
+ */
+public class SOCStringManager {
+    private static SOCStringManager clientManager = null;
+    
+    private ResourceBundle bundle;
+    
+    public SOCStringManager(String bundlePath){
+        bundle = ResourceBundle.getBundle(bundlePath);
+    }
+    
+    public String get(String key){
+        return bundle.getString(key);
+    }
+    public String get(String key, Object ... arguments){
+        return MessageFormat.format(bundle.getString(key), arguments);
+    }
+    
+    public static SOCStringManager getClientManager(){
+        if(clientManager == null)
+            clientManager = new SOCStringManager("soc/client/strings/data");
+        
+        return clientManager;
+    }
+}
