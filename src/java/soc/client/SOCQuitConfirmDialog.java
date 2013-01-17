6fbab50d2a5293ffca45db9f7c2162869b59721d
Jeremy D Monin <jeremy@nand.net>
2013-01-17 07:13:23 -0500

    Followup to uisplit merge: finish attributions

--- src/java/soc/client/SOCQuitConfirmDialog.java
+++ src/java/soc/client/SOCQuitConfirmDialog.java
@@ -2,7 +2,7 @@
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas
  * This file copyright (C) 2007,2008,2010 Jeremy D Monin <jeremy@nand.net>
- * Portions of this file Copyright (C) 2012 Paul Bilnoski <paul@bilnoski.net>
+ * Portions of this file Copyright (C) 2012-2013 Paul Bilnoski <paul@bilnoski.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
