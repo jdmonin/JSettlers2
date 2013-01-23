cf9882430a60ef63b8b9bf9615044316952a9d40
Jeremy D Monin <jeremy@nand.net>
2013-01-23 06:46:59 -0500

    SOCBoardPanel: Lighter previous-pirate-position outline color

--- src/java/soc/client/SOCBoardPanel.java
+++ src/java/soc/client/SOCBoardPanel.java
@@ -1,7 +1,7 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
- * Portions of this file Copyright (C) 2007-2012 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2007-2013 Jeremy D Monin <jeremy@nand.net>
  * Portions of this file Copyright (C) 2012-2013 Paul Bilnoski <paul@bilnoski.net>
  *
  * This program is free software; you can redistribute it and/or
@@ -2753,8 +2753,10 @@ public class SOCBoardPanel extends Canvas implements MouseListener, MouseMotionL
 
         if (! ((pn == -1) && isHilight))
         {
-            if (pn <= -2)
+            if (pn == -2)
                 g.setColor(Color.darkGray);
+            else if (pn == -3)
+                g.setColor(Color.lightGray);
             else if (isHilight)
                 g.setColor(playerInterface.getPlayerColor(pn, false));
             else
