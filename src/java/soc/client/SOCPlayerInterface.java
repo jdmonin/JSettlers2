850789dc1149e8b21410ea0c58534590b142d650
Jeremy D Monin <jeremy@nand.net>
2013-01-28 10:06:01 -0500

    SOCPlayerInterface: If game has scenario.scLongDesc, show popup when client joins

--- src/java/soc/client/SOCPlayerInterface.java
+++ src/java/soc/client/SOCPlayerInterface.java
@@ -34,6 +34,7 @@ import soc.game.SOCPlayer;
 import soc.game.SOCPlayingPiece;
 import soc.game.SOCResourceConstants;
 import soc.game.SOCRoad;
+import soc.game.SOCScenario;
 import soc.game.SOCScenarioEventListener;
 import soc.game.SOCScenarioGameEvent;
 import soc.game.SOCScenarioPlayerEvent;
@@ -44,7 +45,7 @@ import soc.game.SOCVillage;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.EventQueue;  // currently used for javadoc only
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Frame;
@@ -132,6 +133,16 @@ public class SOCPlayerInterface extends Frame
      * @since 1.1.06
      */
     private boolean layoutNotReadyYet;
+
+    /**
+     * True if we've already shown {@link #game}'s scenario's
+     * descriptive text in a popup window when the client joined,
+     * or if the game has no scenario.
+     * Checked in {@link #doLayout()}.
+     * Shown just once, not shown again at {@link #resetBoard(SOCGame, int, int)}.
+     * @since 2.0.00
+     */
+    private boolean didGameScenarioPopupCheck;
 
     //========================================================
     /**
@@ -429,7 +440,8 @@ public class SOCPlayerInterface extends Frame
     private final ClientBridge clientListener;
 
     /**
-     * create a new player interface
+     * Create and show a new player interface.
+     * If the game options have a {@link SOCScenario} description, it will be shown in a popup.
      *
      * @param title  title for this interface - game name
      * @param gd     the player display that spawned us
@@ -529,6 +541,7 @@ public class SOCPlayerInterface extends Frame
          * init is almost complete - when window appears and doLayout is called,
          * it will reset mouse cursor from WAIT_CURSOR to normal (WAIT_CURSOR is
          * set in SOCPlayerClient.startPracticeGame or .guardedActionPerform).
+         * Then, if the game has any scenario description, it will be shown once in a popup.
          */
     }
     
@@ -2357,6 +2370,8 @@ public class SOCPlayerInterface extends Frame
      * Arrange the custom layout. If a player sits down in a 6-player game, will need to
      * {@link #invalidate()} and call this again, because {@link SOCHandPanel} sizes will change.
      * Also, on first call, resets mouse cursor to normal, in case it was WAIT_CURSOR.
+     * On first call, if the game options have a {@link SOCScenario} with any long description,
+     * it will be shown in a popup.
      */
     @Override
     public void doLayout()
@@ -2600,6 +2615,48 @@ public class SOCPlayerInterface extends Frame
             gameDisplay.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
             layoutNotReadyYet = false;
             repaint();
+        }
+
+        if (! didGameScenarioPopupCheck)
+        {
+            final String gameSc = game.getGameOptionStringValue("SC");
+            if (gameSc != null)
+            {
+                SOCScenario sc = SOCScenario.getScenario(gameSc);
+                if (sc != null)
+                {
+                    // TODO also check game for any other _SC_ game opts
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(/*I*/"Game Scenario: "/*18N*/);
+                    sb.append(sc.scDesc);
+                    sb.append('\n');
+
+                    if (sc.scLongDesc != null)
+                    {
+                        sb.append('\n');
+                        sb.append(sc.scLongDesc);
+                        // TODO word wrap
+                    }
+
+                    if (game.vp_winner != SOCGame.VP_WINNER_STANDARD)
+                    {
+                        sb.append('\n');
+                        sb.append(/*I*/"Victory Points to win: "/*18N*/);
+                        sb.append(game.vp_winner);
+                    }
+                    final String scenStr = sb.toString();
+                    EventQueue.invokeLater(new Runnable()
+                    {
+                        public void run()
+                        {
+                            NotifyDialog.createAndShow(getGameDisplay(), SOCPlayerInterface.this, scenStr, null, true);
+                        }
+                    });
+                }
+            }
+
+            didGameScenarioPopupCheck = true;
         }
     }
 
