cd9a7cd24464b5b32b7b7c3f2ee0ff7796f2e1c3
Jeremy D Monin <jeremy@nand.net>
2013-04-02 07:44:50 -0400

    Version.printVersionText: Add "(C)"; "Practice Net" not "Local network"

--- src/java/soc/util/Version.java
+++ src/java/soc/util/Version.java
@@ -195,8 +195,8 @@ public class Version {
   public static void printVersionText(PrintStream out, final String progname)
   {
       out.println(progname + version() +
-          ", build " + buildnum() + ", " + copyright());
-      out.println("Network layer based on code by Cristian Bogdan; local network by Jeremy Monin.");
+          ", build " + buildnum() + ", (C) " + copyright());
+      out.println("Network layer based on code by Cristian Bogdan; Practice Net by Jeremy Monin.");
   }
 
 }
