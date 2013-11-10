12e6fe5ab9a2302fb732d6b6838a503703452951
Jeremy D Monin <jeremy@nand.net>
2013-11-10 13:42:57 -0500

    SOCBankTrade: Javadocs clarify also includes ports

--- src/java/soc/client/SOCDisplaylessPlayerClient.java
+++ src/java/soc/client/SOCDisplaylessPlayerClient.java
@@ -2322,7 +2322,7 @@ public class SOCDisplaylessPlayerClient implements Runnable
     }
 
     /**
-     * the user wants to trade with the bank
+     * the user wants to trade with the bank or a port.
      *
      * @param ga    the game
      * @param give  what is being offered
@@ -2334,7 +2334,7 @@ public class SOCDisplaylessPlayerClient implements Runnable
     }
 
     /**
-     * the user is making an offer to trade
+     * the user is making an offer to trade with another player.
      *
      * @param ga    the game
      * @param offer the trade offer
--- src/java/soc/client/SOCPlayerClient.java
+++ src/java/soc/client/SOCPlayerClient.java
@@ -4998,7 +4998,7 @@ public class SOCPlayerClient
     }
 
     /**
-     * the user wants to trade with the bank
+     * the user wants to trade with the bank or a port.
      *
      * @param ga    the game
      * @param give  what is being offered
@@ -5010,7 +5010,7 @@ public class SOCPlayerClient
     }
 
     /**
-     * the user is making an offer to trade
+     * the user is making an offer to trade with another player.
      *
      * @param ga    the game
      * @param offer the trade offer
