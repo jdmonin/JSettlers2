2683b7e2e21034b1f604be0553f25961f3b6da96
Jeremy D Monin <jeremy@nand.net>
2014-09-20 07:00:23 -0400

    SOCMessageForGame: Javadocs clarity

--- src/java/soc/message/SOCMessageForGame.java
+++ src/java/soc/message/SOCMessageForGame.java
@@ -1,6 +1,6 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
- * This file Copyright (C) 2010,2013 Jeremy D Monin <jeremy@nand.net>
+ * This file Copyright (C) 2010,2013-2014 Jeremy D Monin <jeremy@nand.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,15 +18,15 @@
 package soc.message;
 
 /**
- * This indicates that a {@link SOCMessage} type is always or never about particular games,
- * and contains a game-name field.
+ * This indicates that a {@link SOCMessage} type is always about a particular game
+ * named in the message, or never about that game.
  *<P>
  * Check that {@link #getGame()} is not null before adding to a per-game message queue.
  *<P>
- * Most implementing types will always require a game; some abstract subclasses
- * (such as {@link SOCMessageTemplateMi}) may allow null, leaving the choice to
- * each of their own subclasses.  It's never intended that a specific message type
- * will sometimes hold a game name, and sometimes hold null.
+ * Most implementing types' constructors will always require a game; some abstract
+ * subclasses (such as {@link SOCMessageTemplateMi}) may allow null, leaving the choice
+ * to each of their own subclasses.  Non-abstract message types must always return a
+ * game name from {@link #getGame()}, never null.
  *
  * @since 1.1.11
  * @author Jeremy D Monin
@@ -34,7 +34,8 @@ package soc.message;
 public interface SOCMessageForGame
 {
     /**
-     * @return the name of the game, or <tt>null</tt> if none
+     * @return the name of the game, or (rarely) {@code null} if none.
+     * Must not be {@code null} if message is in per-game structures or code.
      */
     public abstract String getGame();
 
