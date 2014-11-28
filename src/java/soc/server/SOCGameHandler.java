6a2ec75f20a044bb87c31c59f88bd57edc36e7b7
Jeremy D Monin <jeremy@nand.net>
2014-11-28 13:43:26 -0500

    Game option N7C: SOCGameHandler: If game opt N7 also still active, don't announce first city

--- src/java/soc/server/SOCGameHandler.java
+++ src/java/soc/server/SOCGameHandler.java
@@ -2977,7 +2977,14 @@ public class SOCGameHandler extends GameHandler
                     {
                         if (player.isPotentialCity(coord) && (player.getNumPieces(SOCPlayingPiece.CITY) >= 1))
                         {
-                            final boolean houseRuleFirstCity = ga.isGameOptionSet("N7C") && ! ga.hasBuiltCity();
+                            boolean houseRuleFirstCity = ga.isGameOptionSet("N7C") && ! ga.hasBuiltCity();
+                            if (houseRuleFirstCity && ga.isGameOptionSet("N7")
+                                && (ga.getRoundCount() < ga.getGameOptionIntValue("N7")))
+                            {
+                                // If "No 7s for first # rounds" is active, and this isn't its last round, 7s won't
+                                // be rolled soon: Don't announce "Starting next turn, dice rolls of 7 may occur"
+                                houseRuleFirstCity = false;
+                            }
 
                             final SOCCity ci = new SOCCity(player, coord, null);
                             ga.putPiece(ci);  // changes game state and maybe player
