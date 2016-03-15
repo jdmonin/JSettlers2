6f9e851baf06d30259891e318567af29d13fed5e
Jeremy D Monin <jeremy@nand.net>
2016-03-15 07:59:35 -0500

    SOCBoardPanel.debugShowPotentials: Javadocs SOCBoardLarge only

--- src/java/soc/client/SOCBoardPanel.java
+++ src/java/soc/client/SOCBoardPanel.java
@@ -1,7 +1,7 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
- * Portions of this file Copyright (C) 2007-2015 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2007-2016 Jeremy D Monin <jeremy@nand.net>
  * Portions of this file Copyright (C) 2012-2013 Paul Bilnoski <paul@bilnoski.net>
  *
  * This program is free software; you can redistribute it and/or
@@ -684,7 +684,8 @@ public class SOCBoardPanel extends Canvas implements MouseListener, MouseMotionL
 
     /**
      * For debugging, flags to show player 0's potential/legal coordinate sets.
-     * The drawing happens in {@link #drawBoardEmpty(Graphics)}.
+     * Sets flagged as shown are drawn in {@link #drawBoardEmpty(Graphics)}.
+     * Currently implemented only for the sea board layout ({@link SOCBoardLarge}).
      *<P>
      * Stored in same order as piece types:
      *<UL>
