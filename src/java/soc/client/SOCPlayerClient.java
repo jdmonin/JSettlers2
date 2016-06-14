f433619fee9404c1889b4292b2e668b562aef556
Jeremy D Monin <jeremy@nand.net>
2016-06-13 22:15:52 -0400

    SOCPlayerClient.deleteFromGameList: Disable "Show Options" button if last game deleted and list becomes empty; from master/a93c3c7

--- src/java/soc/client/SOCPlayerClient.java
+++ src/java/soc/client/SOCPlayerClient.java
@@ -1,7 +1,7 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
- * Portions of this file Copyright (C) 2007-2015 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2007-2016 Jeremy D Monin <jeremy@nand.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -4485,6 +4485,9 @@ public class SOCPlayerClient extends Applet
                 {
                     serverGames.deleteGame(gameName);  // may not be in there
                 }
+
+                so.setEnabled(false);
+
                 return true;
             }
 
