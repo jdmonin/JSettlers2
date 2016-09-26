189c7610a52d80586fa8fbb1ae05e2439422d760
Jeremy D Monin <jeremy@nand.net>
2016-09-26 06:25:00 -0400

    bin/server.sh: Pass any command line args; Don't run in background; Return exit code in case of startup problems

--- src/bin/server.sh
+++ src/bin/server.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 # sample server startup script: Default port, 8 bots
+# To run server in background, use & when running the script
 
 PORT=8880
 
 echo "Starting the Java Settlers of Catan Server..."
 
 # Params can be given on command line or jsserver.properties in current directory.
+# For parameter info see README.txt or run with --help
+
 # Max connections 30, including the 8 bots;
 # startrobots=8 gives a mix of smart Robots and fast Droids
 
-java -jar JSettlersServer.jar $PORT 30 -Djsettlers.startrobots=8  &
-export SERVER_PID=$!
+java -jar JSettlersServer.jar -Djsettlers.startrobots=8 "$@" $PORT 30
+RC=$?
 
-echo
-echo SERVER_PID=$SERVER_PID
-echo
-
+if [ $RC -ne 0 ]; then
+	echo ""
+	echo "* Server exited; return code $RC"
+	# Remember: interrupt and other signals will give RC >= 128
+fi
+exit $RC
