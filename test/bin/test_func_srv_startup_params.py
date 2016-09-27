a593d83d315e30523673dba03bb255be38ce0961
Jeremy D Monin <jeremy@nand.net>
2016-09-26 21:39:35 -0400

    Automated testing: Structure setup for a functional test (test_func_srv_startup_params.py); work in progress.

---
+++ test/bin/test_func_srv_startup_params.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# JSettlers functional testing: Server startup params
+# Covers command-line params and jsserver.properties files
+# See bottom of file for copyright and license information (GPLv3+).
+
+# File/directory assumptions at runtime:
+# - This script lives in test/bin/
+# - Properties files can be created and deleted in test/tmp/
+# - Server JAR has been built already, to ../../target/JSettlersServer.jar
+
+
+pass
+
+
+# This file is part of the JSettlers project.
+#
+# This file Copyright (C) 2016 Jeremy D Monin <jeremy@nand.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/ .
