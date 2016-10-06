02700c815a907ad6fd675f61ad2a065e21c7fe00
Jeremy D Monin <jeremy@nand.net>
2016-10-06 06:59:55 -0400

    Databases: Correct grants for postgresql 8 (for centos 6/RHEL 6); from master/8a78e0f,635e982

--- src/bin/sql/jsettlers-create-postgres.sql
+++ src/bin/sql/jsettlers-create-postgres.sql
@@ -1,8 +1,12 @@
 -- DB Create script for jsettlers in postgresql.
+-- Run as 'postgres' or another admin user; the admin user will own the
+--   jsettlers db and its objects. For security, the new socuser user won't
+--   have the ability to create or drop db objects, only to work with data.
 -- If you're using the pgAdmin GUI, you can create the socdata db with the
 --   Create Database dialog, then bring up the SQL window (Query Tool) and
 --   paste the rest of this file into there.
 -- Run jsettlers-tables.sql right after this script.
+-- Then run jsettlers-sec-postgres.sql.
 -- See bottom of file for copyright and license information (GPLv3).
 
 CREATE DATABASE socdata
@@ -12,19 +16,17 @@ CREATE DATABASE socdata
 CREATE ROLE socuser LOGIN PASSWORD 'socpass'
    VALID UNTIL 'infinity';
 
--- our tables will be in the postgres 'public' default schema, they aren't created yet
+-- our tables will be in the 'socdata' schema, but they aren't created yet.
+-- so to grant access, postgres 8 requires running another script
+-- (jsettlers-sec-postgres.sql) after jsettlers-tables.sql creates them.
+-- ALTER DEFAULT PRIVILEGES is available in postgres 9+, but CentOS 6 comes with 8.
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-  GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO socuser;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-  GRANT USAGE, SELECT ON SEQUENCES TO socuser;
-
+-- Tested with postgres 8.4 on centos 6
 
 
 -- This file is part of the JSettlers project.
 --
---  This file Copyright (C) 2014 Jeremy D Monin (jeremy@nand.net)
+--  This file Copyright (C) 2014,2016 Jeremy D Monin (jeremy@nand.net)
 --
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
