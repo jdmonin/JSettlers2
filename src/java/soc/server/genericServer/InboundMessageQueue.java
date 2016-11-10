2b394e877c1752c95e8b26c29a6cef17b80c8473
Alessandro D'Ottavio <alessandro.dottavio@gmail.com>
2016-11-10 15:01:48 +0100

    move the SOCInboundMessageQueue in the soc.server.genericServer package and rename in InboundMessageQueue

--- src/java/soc/server/genericServer/InboundMessageQueue.java
+++ src/java/soc/server/genericServer/InboundMessageQueue.java
@@ -2,6 +2,7 @@
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas
  * Portions of this file Copyright (C) 2010,2015-2016 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2016 Alessandro D'Ottavio
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -16,51 +17,49 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
-package soc.server;
+package soc.server.genericServer;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import soc.message.SOCMessage;
-import soc.server.genericServer.Server;
-import soc.server.genericServer.StringConnection;
 
 /**
- * 
+ *
  * this is one of the main class in the messaging infrastructure implemented in the {@link Server}
  * <P>
- * 
+ *
  * The main target of this class is to store all the inbound {@link SOCMessage} in text format received from the server by the client connected
- * and solicit the server to  processing the message 
- * 
+ * and solicit the server to  processing the message
+ *
  * <P>
- * The internal implementation of the {@link SOCInboundMessageQueue} use an internal thread implemented by the inner class {@link Treater} to process the messages received 
+ * The internal implementation of the {@link InboundMessageQueue} use an internal thread implemented by the inner class {@link Treater} to process the messages received
  * in the queue and solicit the {@link Server} using the method {@link Server#processCommand(String, StringConnection)}
- * 
+ *
  * <P>
- * to initialize {@link SOCInboundMessageQueue} use the constructor {@link #SOCInboundMessageQueue(Server)}.
+ * to initialize {@link InboundMessageQueue} use the constructor {@link #SOCInboundMessageQueue(Server)}.
  * This constructor will start only the behavior of the SOCInboundMessageQueue of receive the messages.... <br>
  * To start the processing of the message you have to call {@link #startMessageProcessing()} when the {@link Server} is ready to process the messages.
- * 
+ *
  * <P>
- * Actually this class is used  by the {@link StringConnection} instances and derived instances classes to store the new message received.<br> 
- * To method used to put messages in this queue is {@link #pushMessageInTheQueue(String, StringConnection)} <br>
- * the implementation of the  {@link SOCInboundMessageQueue} use an internal thread-safe implementation queue, so it doesn't need to be synchronized
- * 
+ * Actually this class is used  by the {@link StringConnection} instances and derived instances classes to store the new message received.<br>
+ * The method used to put messages in this queue is {@link #pushMessageInTheQueue(String, StringConnection)} <br>
+ * the implementation of the  {@link InboundMessageQueue} use an internal thread-safe implementation queue, so it doesn't need to be synchronized
+ *
  * <P>
- * The {@link SOCInboundMessageQueue} must be stopped when the {@link Server} owner of this queue is stopped calling {@link #stopMessageProcessing()}.<br>
+ * The {@link InboundMessageQueue} must be stopped when the {@link Server} owner of this queue is stopped calling {@link #stopMessageProcessing()}.<br>
  * This will stop also the {@link Treater} instance of this queue
- * 
+ *
  * <UL>
  * <LI> See {@link SOCMessage} for details of the client/server protocol messaging.
  * <LI> See {@link StringConnection} for details of the client/server communication.
  * <LI> See {@link Server#processCommand(String, StringConnection)} for details of the message processing.
  * </UL>
- *  
- * 
+ *
+ *
  * @author Alessandro D'Ottavio
  * @since 2.0.00
  */
-public class SOCInboundMessageQueue
+public class InboundMessageQueue
 {
 
     /**
@@ -81,10 +80,10 @@ public class SOCInboundMessageQueue
 
     /**
      * Constructor of the SOCInboundMessageQueue.
-     * 
+     *
      * @param server that will use this SOCInboundMessageQueue to store messages and that the SOCInboundMessageQueue will use to treat the messages
      */
-    public SOCInboundMessageQueue(Server server){
+    public InboundMessageQueue(Server server){
         inQueue = new ConcurrentLinkedQueue<MessageData>();
         this.server = server;
     }
@@ -98,16 +97,16 @@ public class SOCInboundMessageQueue
     }
 
     /**
-     * Stop the {@link Treater} internal thread 
-     */ 
+     * Stop the {@link Treater} internal thread
+     */
     public void stopMessageProcessing(){
         treater.stopTreater();
     }
 
     /**
      * put a new message in the inbound queue
-     * 
-     * 
+     *
+     *
      * @param receivedMessage from the connection
      * @param clientConnection that send the message
      */
@@ -121,8 +120,8 @@ public class SOCInboundMessageQueue
      * Internal class user to process the message stored in the {@link #inQueue}
      * <P>
      * the thread can be stopped calling {@link #stopTreater()}
-     * 
-     * 
+     *
+     *
      * @author Alessandro
      */
     class Treater extends Thread
@@ -130,7 +129,7 @@ public class SOCInboundMessageQueue
 
         /**
          * this variable is used to control the processing of the message
-         */ 
+         */
         private boolean processMessage;
 
         public Treater()  // Server parameter is also passed in, since this is an inner class
@@ -156,9 +155,9 @@ public class SOCInboundMessageQueue
                     {
                         server.processCommand(messageData.getStringMessage(), messageData.getClientConnection());
                     }else{
-                            Thread.sleep(10);                            
+                            Thread.sleep(10);
                     }
-                      
+
                 }
                 catch (Exception e)
                 {
@@ -196,7 +195,7 @@ public class SOCInboundMessageQueue
             return clientConnection;
         }
 
-    } 
+    }
 
 
 }
