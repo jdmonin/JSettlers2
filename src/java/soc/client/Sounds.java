9babc04b64d67f78fdcf089145110e9b8e030d82
Jeremy D Monin <jeremy@nand.net>
2017-07-11 23:46:14 -0400

    Sounds +chime(..), with volume fading out to 0

--- src/java/soc/client/Sounds.java
+++ src/java/soc/client/Sounds.java
@@ -35,7 +35,47 @@ public class Sounds
     /** Sampling rate */
     private static final float SAMPLE_RATE_HZ = 22050f;
 
+    /** Major-scale "A" */
+    private static final int CHIME_A_HZ = 2 * 880;
+
     private static final double PI_X_2 = 2.0 * Math.PI;
+
+    // plan: generate CHIME_A_HZ, amplitude 0.8-0.9 for .01sec, then .9 to 0 for .18sec.
+
+    /**
+     * Generate and play a chime, with volume fading out to 0.
+     * @param hz  Tone in Hertz
+     * @param msec  Duration in milliseconds
+     * @param vol  Volume (max is 1.0)
+     * @throws LineUnavailableException
+     */
+    public static void chime(int hz, int msec, double vol)
+        throws LineUnavailableException
+    {
+        // TODO 2 parts: attack for first 10msec, then release for rest of msec
+
+        byte[] buf = new byte[1];
+        AudioFormat af = new AudioFormat
+            (SAMPLE_RATE_HZ, // sampleRate
+             8,           // sampleSizeInBits
+             1,           // channels
+             true,        // signed
+             false);      // bigEndian
+        SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
+        sdl.open(af);
+        sdl.start();
+        final int imax = (int) ((msec * SAMPLE_RATE_HZ) / 1000);
+        for (int i = imax; i > 0; --i)
+        {
+            double angle = i / (SAMPLE_RATE_HZ / hz) * PI_X_2;
+            buf[0] = (byte)(Math.sin(angle) * ((127.0 * vol * i) / imax));
+            sdl.write(buf,0,1);
+        }
+        sdl.drain();
+        sdl.stop();
+        sdl.close();
+
+    }
 
     /**
      * Generate and play a constant tone.
@@ -78,6 +118,11 @@ public class Sounds
     {
         try
         {
+            chime(CHIME_A_HZ, 180, .9);
+
+            /*
+            // from stack overflow example:
+
             tone(1000,100, 1.0);
             Thread.sleep(1000);
             tone(100,1000, 1.0);
@@ -87,6 +132,7 @@ public class Sounds
             tone(400,500, 1.0);
             Thread.sleep(1000);
             tone(400,500, 0.2);
+            */
 
         } catch (Exception e) {
             // LineUnavailableException, InterruptedException
