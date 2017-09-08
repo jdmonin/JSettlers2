205b0c2ef4e2cf45ea8a69cf3386503c229a7439
Jeremy D Monin <jeremy@nand.net>
2017-09-08 06:52:01 -0400

    PR#35 TestResourceSet: Add copyright from commit author info, add standard GPLv3 header

--- src/test/java/soctest/TestResourceSet.java
+++ src/test/java/soctest/TestResourceSet.java
@@ -1,3 +1,23 @@
+/**
+ * Java Settlers - An online multiplayer version of the game Settlers of Catan
+ * This file Copyright (C) 2017 Ruud Poutsma <rtimon@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The maintainer of this program can be reached at jsettlers@nand.net
+ **/
+
 package soctest;
 
 import soc.game.SOCResourceConstants;
