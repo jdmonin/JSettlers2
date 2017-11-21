f9b2c7fee0a852a6476f343c7725054d8570f6d1
Jeremy D Monin <jeremy@nand.net>
2017-11-21 00:56:25 -0500

    socweb.server.Main: Standard GPLv3 header; reach maintainer @ github

--- src/main/java/socweb/server/Main.java
+++ src/main/java/socweb/server/Main.java
@@ -1,11 +1,22 @@
 /*
- * This file is part of the Java Settlers Web App.
+ * This file is part of the Java Settlers Server Web App.
  *
  * This file Copyright (C) 2017 Jeremy D Monin <jeremy@nand.net>
  *
- * Open-source license: TBD
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
  *
- * The maintainer of this program can be reached at https://github.com/jdmonin/jsettlers-webapp
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The maintainer of this program can be reached at https://github.com/jdmonin/JSettlers2
  */
 
 package socweb.server;
