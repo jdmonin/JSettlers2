916e09d59cdb1e33d5ee377623e291555394823b
Jeremy D Monin <jeremy@nand.net>
2018-04-28 09:23:15 -0400

    SOCAcceptOffer misc javadocs; from stable-1.x.xx/e897a89

--- src/main/java/soc/message/SOCAcceptOffer.java
+++ src/main/java/soc/message/SOCAcceptOffer.java
@@ -31,11 +31,12 @@ import java.util.StringTokenizer;
  *<UL>
  * <LI> Message to server is in response to a {@link SOCMakeOffer} sent earlier this turn to client.
  * <LI> Server's response (announced to game) is {@link SOCPlayerElement}s, {@link SOCGameServerText},
- *      then the {@code AcceptOffer}, then {@link SOCClearOffer}s.
+ *      {@code AcceptOffer}, then {@link SOCClearOffer}s.
  *</UL>
  *<P>
  * Before v2.0.00 the server announced the {@code SOCClearOffer}s before {@code SOCAcceptOffer}. The old
  * non-robot clients ignored that {@code SOCAcceptOffer}, so changing the order has no effect on them.
+ *
  * @author Robert S. Thomas
  * @see SOCRejectOffer
  */
@@ -61,7 +62,7 @@ public class SOCAcceptOffer extends SOCMessage
     private int offering;
 
     /**
-     * Create a AcceptOffer message.
+     * Create an AcceptOffer message.
      *
      * @param ga  the name of the game
      * @param ac  the player number of the accepting player.
