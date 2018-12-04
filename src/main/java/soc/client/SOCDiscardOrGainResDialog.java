071a9a483c379d50bf3a868b111dd46d779ba713
Jeremy D Monin <jeremy@nand.net>
2018-12-04 18:09:26 -0500

    SOCMonopolyDialog: Add resource type ColorSquares next to buttons; convert to Swing to do so; Swing shouldn't use AWTToolTip

--- src/main/java/soc/client/SOCDiscardOrGainResDialog.java
+++ src/main/java/soc/client/SOCDiscardOrGainResDialog.java
@@ -1,7 +1,7 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
- * Portions of this file Copyright (C) 2007-2009,2011-2014,2017 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2007-2009,2011-2014,2017-2018 Jeremy D Monin <jeremy@nand.net>
  * Portions of this file Copyright (C) 2012 Paul Bilnoski <paul@bilnoski.net>
  *
  * This program is free software; you can redistribute it and/or
@@ -165,6 +165,7 @@ class SOCDiscardOrGainResDialog extends Dialog implements ActionListener, MouseL
         {
             // On OSX: We must use the wrong color, then change it, in order to
             // not use AWTToolTips (redraw problem for button enable/disable).
+            // See also SOCMonopolyDialog which uses the same tooltip-avoidance code.
             Color sqColor;
             if (SOCPlayerClient.isJavaOnOSX)
                 sqColor = Color.WHITE;
--- src/main/java/soc/client/SOCMonopolyDialog.java
+++ src/main/java/soc/client/SOCMonopolyDialog.java
@@ -20,23 +20,25 @@
  **/
 package soc.client;
 
-import java.awt.Button;
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Label;
-import java.awt.LayoutManager;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JRootPane;
+import javax.swing.WindowConstants;
 
 
 @SuppressWarnings("serial")
@@ -71,28 +73,78 @@ class SOCMonopolyDialog extends JDialog implements ActionListener, Runnable
         cpane.setBackground(null);  // inherit from parent
         cpane.setForeground(null);
 
-        cpane.setLayout(new GridLayout(6, 1, 10, 10));  // label + 1 row per button
-
         msg = new JLabel(strings.get("dialog.mono.please.pick.resource"), JLabel.CENTER);
             // "Please pick a resource to monopolize."
-        add(msg);
+        add(msg, BorderLayout.PAGE_START);  // NORTH
+
+        // The actual content of this dialog is btnsPane, a narrow stack of 5 rows, 1 per resource type.
+        // Each row is a JPanel with 1 resource type's color and button.
+        // This stack of rows is centered horizontally in the larger container,
+        // and doesn't fill the entire width. Since the content pane's BorderLayout wants to
+        // stretch things to fill its center, to leave space on the left and right
+        // we wrap btnsPane in a larger btnsContainer ordered horizontally.
+
+        final JPanel btnsContainer = new JPanel();
+        btnsContainer.setLayout(new BoxLayout(btnsContainer, BoxLayout.X_AXIS));
+        btnsContainer.setBackground(null);
+        btnsContainer.setForeground(null);
+
+        // In center of btnContainer, the stack of resource buttons:
+        final JPanel btnsPane = new JPanel()
+        {
+            /**
+             * Override to prevent some unwanted extra width, because default max is 32767 x 32767
+             * and BoxLayout adds some proportion of that, based on its overall container width
+             * beyond btnsPane's minimum/preferred width.
+             */
+            public Dimension getMaximumSize() { return getPreferredSize(); }
+        };
+        btnsPane.setLayout(new BoxLayout(btnsPane, BoxLayout.Y_AXIS));
+        btnsPane.setBackground(null);
+        btnsPane.setForeground(null);
+        btnsPane.setAlignmentX(CENTER_ALIGNMENT);  // center btnsPane within entire content pane
+        btnsPane.add(Box.createRigidArea(new Dimension(9, 9)));  // space between prompt label and resource rows
 
         rsrcBut = new JButton[5];
-
-        rsrcBut[0] = new JButton(strings.get("resources.clay"));   // "Clay"
-        rsrcBut[1] = new JButton(strings.get("resources.ore"));    // "Ore"
-        rsrcBut[2] = new JButton(strings.get("resources.sheep"));  // "Sheep"
-        rsrcBut[3] = new JButton(strings.get("resources.wheat"));  // "Wheat"
-        rsrcBut[4] = new JButton(strings.get("resources.wood"));   // "Wood"
-
-        for (int i = 0; i < 5; i++)
+        final String[] rsrcStr
+            = { "resources.clay", "resources.ore", "resources.sheep", "resources.wheat", "resources.wood" };
+        for (int i = 0; i < 5; ++i)
         {
-            add(rsrcBut[i]);
-            rsrcBut[i].addActionListener(this);
+            // Need to use the wrong color, then change it, in order to
+            // not use AWTToolTips (redraw problem when in a JDialog).
+            // See also SOCDiscardOrGainResDialog which uses the same tooltip-avoidance code.
+            final Color sqColor = ColorSquare.RESOURCE_COLORS[i];
+
+            ColorSquareLarger sq = new ColorSquareLarger(Color.WHITE);
+            sq.setBackground(sqColor);
+
+            JButton b = new JButton(strings.get(rsrcStr[i]));
+            b.addActionListener(this);
+            rsrcBut[i] = b;
+
+            JPanel row = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            row.setBackground(null);
+            row.setForeground(null);
+            row.setAlignmentY(CENTER_ALIGNMENT);
+
+            row.add(sq);
+            row.add(b);
+            btnsPane.add(row);
         }
+
+        btnsContainer.add(Box.createHorizontalGlue());
+        btnsContainer.add(btnsPane);
+        btnsContainer.add(Box.createHorizontalGlue());
+
+        add(btnsContainer, BorderLayout.CENTER);
 
         pack();
         setLocationRelativeTo(pi);  // will center dialog in game window
+
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) { }  // don't close
+        });
     }
 
     /**
