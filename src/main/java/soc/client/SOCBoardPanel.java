0bc971b498dda78b01cfba4a451acef2697d0b03
Jeremy D Monin <jeremy@nand.net>
2019-02-06 23:23:29 -0500

    SOCBoardPanel (c)+2019 for drawFortress, fortressX, fortressY

--- src/main/java/soc/client/SOCBoardPanel.java
+++ src/main/java/soc/client/SOCBoardPanel.java
@@ -1,7 +1,7 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
- * Portions of this file Copyright (C) 2007-2018 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2007-2019 Jeremy D Monin <jeremy@nand.net>
  * Portions of this file Copyright (C) 2012-2013 Paul Bilnoski <paul@bilnoski.net>
  * Portions of this file Copyright (C) 2017 Ruud Poutsma <rtimon@gmail.com>
  *
