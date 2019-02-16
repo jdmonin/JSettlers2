f5a86cb864e29a0a16a940bd0e8ae0f08dca1f89
Jeremy D Monin <jeremy@nand.net>
2019-02-16 17:35:19 -0500

    Client refactor: SOCDialog with common Swing dialog setup code from SOCMonopolyDialog

--- src/main/java/soc/client/SOCMonopolyDialog.java
+++ src/main/java/soc/client/SOCMonopolyDialog.java
@@ -20,35 +20,21 @@
  **/
 package soc.client;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRootPane;
-import javax.swing.WindowConstants;
 
 
 @SuppressWarnings("serial")
-/*package*/ class SOCMonopolyDialog extends JDialog implements ActionListener, Runnable
+/*package*/ class SOCMonopolyDialog
+    extends SOCDialog implements ActionListener
 {
     final JButton[] rsrcBut;
-    /** Prompt message. Text alignment is centered. */
-    final JLabel msg;
-    final SOCPlayerInterface pi;
 
     /** i18n text strings; will use same locale as SOCPlayerClient's string manager.
      *  @since 2.0.00 */
@@ -61,52 +47,20 @@ import javax.swing.WindowConstants;
      */
     public SOCMonopolyDialog(SOCPlayerInterface pi)
     {
-        super(pi, strings.get("spec.dcards.monopoly"), true);  // "Monopoly"
+        super(pi, strings.get("spec.dcards.monopoly"), strings.get("dialog.mono.please.pick.resource"), false);
+            // title: "Monopoly"  prompt: "Please pick a resource to monopolize."
 
-        this.pi = pi;
-
-        final JRootPane rpane = getRootPane();
-        final Container cpane = getContentPane();
-
-        rpane.setBorder(BorderFactory.createEmptyBorder(5, 20, 20, 20));
-        rpane.setBackground(SOCPlayerInterface.DIALOG_BG_GOLDENROD);
-        rpane.setForeground(Color.BLACK);
-        cpane.setBackground(null);  // inherit from parent
-        cpane.setForeground(null);
-
-        msg = new JLabel(strings.get("dialog.mono.please.pick.resource"), JLabel.CENTER);
-            // "Please pick a resource to monopolize."
-        add(msg, BorderLayout.PAGE_START);  // NORTH
+        getRootPane().setBorder(BorderFactory.createEmptyBorder(5, 20, 20, 20));
 
         // The actual content of this dialog is btnsPane, a narrow stack of 5 rows, 1 per resource type.
         // Each row has 1 resource type's colorsquare and a button with its name.
-        // This stack of rows is centered horizontally in the larger container,
-        // and doesn't fill the entire width. Since the content pane's BorderLayout wants to
-        // stretch things to fill its center, to leave space on the left and right
-        // we wrap btnsPane in a larger btnsContainer ordered horizontally.
+        // This stack of rows is centered horizontally in a larger container,
+        // and doesn't fill the entire width.
 
-        final JPanel btnsContainer = new JPanel();
-        btnsContainer.setLayout(new BoxLayout(btnsContainer, BoxLayout.X_AXIS));
-        btnsContainer.setBackground(null);
-        btnsContainer.setForeground(null);
-
-        // In center of btnContainer, the stack of resource buttons:
+        JPanel btnsPane = getMiddlePanel();
         final GridBagLayout gbl = new GridBagLayout();
         final GridBagConstraints gbc = new GridBagConstraints();
-        final JPanel btnsPane = new JPanel(gbl)
-        {
-            /**
-             * Override to prevent some unwanted extra width, because default max is 32767 x 32767
-             * and parent's BoxLayout adds some proportion of that, based on its overall container width
-             * beyond btnsPane's minimum/preferred width.
-             */
-            public Dimension getMaximumSize() { return getPreferredSize(); }
-        };
-        btnsPane.setBackground(null);
-        btnsPane.setForeground(null);
-        btnsPane.setAlignmentX(CENTER_ALIGNMENT);  // center btnsPane within entire content pane
-        btnsPane.setBorder
-            (BorderFactory.createEmptyBorder(9, 0, 0, 0));  // space between prompt label and resource rows
+        btnsPane.setLayout(gbl);
 
         rsrcBut = new JButton[5];
         final String[] rsrcStr
@@ -135,19 +89,7 @@ import javax.swing.WindowConstants;
             btnsPane.add(b);
         }
 
-        btnsContainer.add(Box.createHorizontalGlue());
-        btnsContainer.add(btnsPane);
-        btnsContainer.add(Box.createHorizontalGlue());
-
-        add(btnsContainer, BorderLayout.CENTER);
-
         pack();
-        setLocationRelativeTo(pi);  // will center dialog in game window
-
-        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent we) { }  // don't close
-        });
     }
 
     /**
@@ -174,27 +116,17 @@ import javax.swing.WindowConstants;
             if (target == rsrcBut[i])
             {
                 /**
-                 * Note: This only works if SOCResourceConstants.CLAY == 1 and so on, in same order as rsrcBut buttons
+                 * Note: This works because SOCResourceConstants.CLAY == 1 and so on, in same order as rsrcBut buttons
                  */
-                pi.getClient().getGameManager().pickResourceType(pi.getGame(), i + 1);
+                playerInterface.getClient().getGameManager().pickResourceType(playerInterface.getGame(), i + 1);
                 dispose();
 
                 break;
             }
         }
         } catch (Throwable th) {
-            pi.chatPrintStackTrace(th);
+            playerInterface.chatPrintStackTrace(th);
         }
-    }
-
-    /**
-     * Run method, for convenience with {@link java.awt.EventQueue#invokeLater(Runnable)}.
-     * This method just calls {@link #setVisible(boolean) setVisible(true)}.
-     * @since 2.0.00
-     */
-    public void run()
-    {
-        setVisible(true);
     }
 
 }
