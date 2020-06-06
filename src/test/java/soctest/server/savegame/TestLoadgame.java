aececfc3c4b14a6f64e047b973614cf3786016b2
Jeremy D Monin <jeremy@nand.net>
2020-06-06 19:45:58 -0400

    savegame: unit tests: 6-player game/Special Building Phase, artifact test6p-sbp.game.json; refactor common code -> checkPlayerData, fillSeatsForResume; misc other adjustments

--- src/test/java/soctest/server/savegame/TestLoadgame.java
+++ src/test/java/soctest/server/savegame/TestLoadgame.java
@@ -78,6 +78,87 @@ public class TestLoadgame
         return GameLoaderJSON.loadGame(f);
     }
 
+    /**
+     * Check savegame/game data against expected contents.
+     * @param names Player names
+     * @param totalVP Players' total VP
+     * @param resources Players' resources (clay, ore, sheep, wheat, wood)
+     * @param pieceCounts Players' piece counts (roads, settlements, and cities remaining; knights/soldiers played)
+     */
+    private static void checkPlayerData
+        (final SavedGameModel sgm, final String[] names, final int[] totalVP,
+         final int[][] resources, final int[][] pieceCounts)
+    {
+        final SOCGame ga = sgm.getGame();
+        assertEquals("maxPlayers", names.length, ga.maxPlayers);
+
+        for (int pn = 0; pn < 4; ++pn)
+        {
+            final SavedGameModel.PlayerInfo pi = sgm.playerSeats[pn];
+            final SOCPlayer pl = ga.getPlayer(pn);
+            assertEquals(names[pn], pi.name);
+            assertEquals(names[pn], pl.getName());
+            assertEquals(totalVP[pn], pi.totalVP);
+            assertEquals(totalVP[pn], pl.getTotalVP());
+
+            final SOCResourceSet rs = (resources[pn] != null)
+                ? new SOCResourceSet(resources[pn])
+                : new SOCResourceSet();
+            assertTrue(rs.equals(pl.getResources()));
+
+            for (int ptype = SOCPlayingPiece.ROAD; ptype <= SOCPlayingPiece.CITY; ++ptype)
+                assertEquals(pieceCounts[pn][ptype], pl.getNumPieces(ptype));
+                // ROADS, SETTLEMENTS, CITIES == 0, 1, 2: tested in TestPlayingPiece
+            assertEquals(pieceCounts[pn][3], pl.getNumKnights());
+
+            // TODO piece locations, etc
+
+            final boolean isVacant = (names[pn] == null);
+            assertEquals(isVacant, pi.isSeatVacant);
+            assertEquals(isVacant, ga.isSeatVacant(pn));
+            if (isVacant)
+                continue;
+
+            final boolean isBot = (names[pn].startsWith("robot") || names[pn].startsWith("droid"));
+                // not a completely rigorous test, but works for our savegame artifacts
+            assertEquals(isBot, pi.isRobot);
+            assertEquals(isBot, pl.isRobot());
+        }
+
+    }
+
+    /**
+     * Do the required {@link SOCGameListAtServer} setup for this loaded game
+     * so {@link SavedGameModel#resumePlay(boolean)} won't complain.
+     * Doesn't actually call {@code resumePlay(..)}.
+     */
+    private static void fillSeatsForResume(final SavedGameModel sgm)
+    {
+        assertNotNull(sgm.findSeatsNeedingBots());
+
+        final SOCGame ga = sgm.getGame();
+        final String gaName = ga.getName();
+
+        SavedGameModel.glas.addGame(gaName, null, false);
+            // this isn't the server-side call, but that one's much more work to set up for
+
+        for (int pn = 0; pn < ga.maxPlayers; ++pn)
+        {
+            final String pName = sgm.playerSeats[pn].name;
+            boolean isVacant = (null == pName);
+            assertEquals(isVacant, ga.isSeatVacant(pn));
+            if (isVacant)
+                continue;
+
+            // GameList membership required by findSeatsNeedingBots
+            assertFalse(SavedGameModel.glas.isMember(pName, gaName));
+            StringConnection sc = new StringConnection();
+            sc.setData(pName);
+            SavedGameModel.glas.addMember(sc, gaName);
+            assertTrue(SavedGameModel.glas.isMember(pName, gaName));
+        }
+    }
+
     @Test
     public void testBasicLoading()
         throws IOException
@@ -92,48 +173,18 @@ public class TestLoadgame
         assertEquals("gamestate", SOCGame.PLAY1, sgm.gameState);
         assertEquals("gamestate", SOCGame.LOADING, ga.getGameState());
 
+        assertEquals(4, sgm.playerSeats.length);
+        assertEquals(4, ga.maxPlayers);
+        assertFalse(ga.hasSeaBoard);
+
         assertEquals(SOCBoard.BOARD_ENCODING_ORIGINAL, ga.getBoard().getBoardEncodingFormat());
         assertEquals("robberHex", 155, ga.getBoard().getRobberHex());
 
-        assertEquals(4, sgm.playerSeats.length);
-        assertEquals(4, ga.maxPlayers);
         final String[] NAMES = {null, "robot 4", "robot 2", "debug"};
         final int[] TOTAL_VP = {0, 3, 2, 2};
         final int[][] RESOURCES = {null, {0, 1, 0, 2, 0}, {2, 2, 0, 0, 0}, {1, 3, 1, 0, 1}};
         final int[][] PIECE_COUNTS = {{15, 5, 4, 0}, {13, 4, 3, 0}, {13, 3, 4, 0}, {12, 3, 4, 0}};
-
-        for (int pn = 0; pn < 4; ++pn)
-        {
-            final SavedGameModel.PlayerInfo pi = sgm.playerSeats[pn];
-            final SOCPlayer pl = ga.getPlayer(pn);
-            assertEquals(NAMES[pn], pi.name);
-            assertEquals(NAMES[pn], pl.getName());
-            assertEquals(TOTAL_VP[pn], pi.totalVP);
-            assertEquals(TOTAL_VP[pn], pl.getTotalVP());
-
-            final SOCResourceSet rs = (RESOURCES[pn] != null)
-                ? new SOCResourceSet(RESOURCES[pn])
-                : new SOCResourceSet();
-            assertTrue(rs.equals(pl.getResources()));
-
-            for (int ptype = SOCPlayingPiece.ROAD; ptype <= SOCPlayingPiece.CITY; ++ptype)
-                assertEquals(PIECE_COUNTS[pn][ptype], pl.getNumPieces(ptype));
-                // ROADS, SETTLEMENTS, CITIES == 0, 1, 2: tested in TestPlayingPiece
-            assertEquals(PIECE_COUNTS[pn][3], pl.getNumKnights());
-
-            // TODO piece locations, etc
-
-            final boolean isVacant = (NAMES[pn] == null);
-            assertEquals(isVacant, pi.isSeatVacant);
-            assertEquals(isVacant, ga.isSeatVacant(pn));
-            if (isVacant)
-                continue;
-
-            final boolean isBot = ! ("debug".equals(NAMES[pn]));
-                // not a sufficient condition in all savegame artifacts, but works here
-            assertEquals(isBot, pi.isRobot);
-            assertEquals(isBot, pl.isRobot());
-        }
+        checkPlayerData(sgm, NAMES, TOTAL_VP, RESOURCES, PIECE_COUNTS);
     }
 
     /**
@@ -160,31 +211,14 @@ public class TestLoadgame
         assertEquals("game name", GAME_NAME, sgm.gameName);
         assertTrue(ga.isSeatVacant(0));
 
-        assertNotNull(sgm.findSeatsNeedingBots());
-        SavedGameModel.glas.addGame(GAME_NAME, null, false);
-            // this isn't the server-side call, but that one's much more work to set up for
-
-        for (int pn = 1; pn < 4; ++pn)
-        {
-            final String pName = sgm.playerSeats[pn].name;
-
-            assertFalse(ga.isSeatVacant(pn));
-
-            // GameList membership required by findSeatsNeedingBots
-            assertFalse(SavedGameModel.glas.isMember(pName, GAME_NAME));
-            StringConnection sc = new StringConnection();
-            sc.setData(pName);
-            SavedGameModel.glas.addMember(sc, GAME_NAME);
-            assertTrue(SavedGameModel.glas.isMember(pName, GAME_NAME));
-        }
-
+        fillSeatsForResume(sgm);
         sgm.resumePlay(true);
         assertEquals("gamestate", SOCGame.PLAY1, ga.getGameState());
     }
 
     /** When loading a game that's over, should call {@link SavedGameModel#resumePlay(boolean)} successfully */
     @Test
-    public void testLoadGameOverResume()
+    public void testLoadResumeGameOver()
         throws IOException
     {
         final SavedGameModel sgm = load("classic-over.game.json");
@@ -197,6 +231,35 @@ public class TestLoadgame
         assertEquals("gamestate", SOCGame.OVER, ga.getGameState());
         assertEquals(2, ga.getPlayerWithWin().getPlayerNumber());
         assertNull(ga.savedGameModel);
+    }
+
+    /** Test loading a 6-player game and its Special Building Phase, including {@code resumePlay(..)}. */
+    @Test
+    public void testLoad6PlayerSBP()
+        throws IOException
+    {
+        final SavedGameModel sgm = load("test6p-sbp.game.json");
+        final SOCGame ga = sgm.getGame();
+
+        assertEquals("game name", "test6p-sbp", sgm.gameName);
+        assertEquals(1, ga.getCurrentPlayerNumber());
+        assertEquals("gamestate", SOCGame.SPECIAL_BUILDING, sgm.gameState);
+        assertEquals("should be 6 players", 6, sgm.playerSeats.length);
+        assertEquals("should be 6 players", 6, ga.maxPlayers);
+        assertFalse(ga.hasSeaBoard);
+
+        assertEquals(SOCBoard.BOARD_ENCODING_6PLAYER, ga.getBoard().getBoardEncodingFormat());
+        assertEquals("robberHex", 21, ga.getBoard().getRobberHex());
+
+        final String[] NAMES = {null, "p", "droid 1", "robot 2", "robot 4", "debug"};
+        final int[] TOTAL_VP = {0, 2, 2, 2, 2, 2};
+        final int[][] RESOURCES = {null, {2, 0, 1, 1, 1}, null, {0, 1, 3, 0, 2}, {0, 0, 2, 0, 0}, {4, 0, 0, 0, 3}};
+        final int[][] PIECE_COUNTS = {{15, 5, 4, 0}, {13, 3, 4, 0}, {12, 3, 4, 0}, {13, 3, 4, 1}, {12, 3, 4, 0}, {13, 3, 4, 0}};
+        checkPlayerData(sgm, NAMES, TOTAL_VP, RESOURCES, PIECE_COUNTS);
+
+        fillSeatsForResume(sgm);
+        sgm.resumePlay(true);
+        assertEquals("gamestate", SOCGame.SPECIAL_BUILDING, ga.getGameState());
     }
 
 }
