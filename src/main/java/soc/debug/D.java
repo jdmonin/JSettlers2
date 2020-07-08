8664a8fd1e83ac4db3d322bace0f1e36d4cfccfc
STAC Project <>
2020-07-08 19:55:17 -0400

    Upstreamed from STAC Project fork https://github.com/sorinMD/StacSettlers: Extend soc.debug / disableDebug: 4 debug levels INFO, WARNING, ERROR, FATAL

--- src/main/java/soc/debug/D.java
+++ src/main/java/soc/debug/D.java
@@ -2,6 +2,7 @@
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
  * Portions of this file Copyright (C) 2007-2009,2012,2014,2020 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2017-2018 Strategic Conversation (STAC Project) https://www.irit.fr/STAC/
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,14 +25,57 @@ package soc.debug;
  * Debug output; can be switched on and off.  All output goes to System.out.
  * soc.debug.D and {@link soc.disableDebug.D} have the same interface, to easily switch
  * debug on and off per class.
+ *
+ * Extended with 4 levels of importance: {@link #INFO}, {@link #WARNING}, {@link #ERROR}, {@link #FATAL};
+ * Depending on the level, call one of the debug methods to print out.
  */
 public class D
 {
+    /**
+     * Print out everything
+     */
+    public static final int INFO = 0;
+    /**
+     * Print out warnings or above
+     */
+    public static final int WARNING = 1;
+    /**
+     * Print out errors or fatals
+     */
+    public static final int ERROR = 2;
+    /**
+     * Print out fatals only. NOTE: despite the name, fatals are exceptions that may or may not cause the application to crash
+     */
+    public static final int FATAL = 3;
+
+    /**
+     * The debug level one of: {@link #INFO}, {@link #WARNING}, {@link #ERROR}, {@link #FATAL}
+     * Default set to WARNING.
+     */
+    static private int level = WARNING;
+
     static public final boolean ebugOn = true;
     static private boolean enabled = true;
 
     /**
-     * DOCUMENT ME!
+     * Set the debug level to one of: {@link #INFO}, {@link #WARNING}, {@link #ERROR}, {@link #FATAL}
+     * The default is WARNING.
+     */
+    public static void setLevel(int l){
+        level = l;
+    }
+
+    /**
+     * Get the current debug level (one of: {@link #INFO}, {@link #WARNING}, {@link #ERROR}, {@link #FATAL})
+     * The default is WARNING.
+     * @return
+     */
+    public static int ebug_level(){
+        return level;
+    }
+
+    /**
+     * Enable debugging - start producing output.
      */
     public static final void ebug_enable()
     {
@@ -39,7 +83,7 @@ public class D
     }
 
     /**
-     * DOCUMENT ME!
+     * Disable debugging - stop producing output.
      */
     public static final void ebug_disable()
     {
@@ -48,6 +92,7 @@ public class D
 
     /**
      * Is debug currently enabled?
+     * @return  true if debugging is currently enabled
      * @since 1.1.00
      */
     public static final boolean ebugIsEnabled()
@@ -87,6 +132,56 @@ public class D
     public static final void ebugPrint(String text)
     {
         if (enabled)
+        {
+            System.out.print(text);
+        }
+    }
+
+    /**
+     * Debug-println this info text;
+     *
+     * @param text DOCUMENT ME!
+     */
+    public static final void ebugPrintlnINFO(String text)
+    {
+        if (enabled && level == INFO)
+        {
+            System.out.println(text);
+        }
+    }
+
+    /**
+     * Debug-println this info text;
+     *
+     * @param text DOCUMENT ME!
+     */
+    public static final void ebugPrintlnINFO(String prefix, String text)
+    {
+        if (enabled && level == INFO)
+        {
+            System.out.println(prefix + ":" + text);
+        }
+    }
+
+    /**
+     * Debug-println;
+     */
+    public static final void ebugPrintlnINFO()
+    {
+        if (enabled && level == INFO)
+        {
+            System.out.println();
+        }
+    }
+
+    /**
+     * Debug-print this info text;
+     *
+     * @param text DOCUMENT ME!
+     */
+    public static final void ebugPrintINFO(String text)
+    {
+        if (enabled && level == INFO)
         {
             System.out.print(text);
         }
@@ -155,6 +250,97 @@ public class D
             cause.printStackTrace(System.out);
         }
         System.out.println("-- Exception ends: " + ex.getClass().getName() + " --");
+    }
+
+    /**
+     * Debug-println this fatal error.
+     * If debug is enabled, print the stack trace of this exception
+     * @param ex Exception or other Throwable.  If null, will create an exception
+     *           in order to force a stack trace.
+     * @param prefixMsg Message for {@link #ebugPrintlnINFO(String)} above the exception,
+     *                  or null; will print as:
+     *                  prefixMsg + " - " + ex.toString
+     */
+    public static final void ebugFATAL(Throwable ex, String prefixMsg)
+    {
+        if (! enabled)
+        {
+            return;
+        }
+
+        if (ex == null)
+        {
+            try
+            {
+                int x = 0;
+                int y = x / 0;
+                System.out.print(y);
+            } catch (Throwable th)
+            {
+                ex = th;
+            }
+        }
+        if (prefixMsg != null)
+            ebugPrintlnINFO(prefixMsg + " - " + ex.toString());
+        System.out.println("-- Exception stack trace begins -- Thread: " + Thread.currentThread().getName());
+        ex.printStackTrace(System.out);
+
+        /**
+         * Look for cause(s) of exception
+         */
+        Throwable prev = ex;
+        for ( Throwable cause = prev.getCause();    // NOTE: getCause is 1.4+
+              ((cause != null) && (cause != prev));
+               prev = cause )
+        {
+            System.out.println("** --> Nested cause exception: **");
+            cause.printStackTrace(System.out);
+        }
+        System.out.println("-- Exception ends: " + ex.getClass().getName() + " --");
+    }
+
+    /**
+     * Debug-println this warning text;
+     * @param text Text to debug-print
+     */
+    public static final void ebugWARNING(String text) {
+        if (enabled && level <= WARNING)
+        {
+            System.out.println("WARN: " + text);
+        }
+    }
+
+    /**
+     * Debug-println this warning text;
+     * @param text Text to debug-print
+     */
+    public static final void ebugWARNING(String prefix, String text) {
+        if (enabled && level <= WARNING)
+        {
+            System.out.println("WARN: " + prefix + " " + text);
+        }
+    }
+
+    /**
+     * Debug-println this error text;
+     * @param text Text to debug-print
+     */
+    public static final void ebugERROR(String text) {
+        if (enabled && level <= ERROR)
+        {
+            System.out.println("ERR: " + text);
+        }
+    }
+
+    /**
+     * Debug-println this error text;
+     * @param text Text to debug-print
+     */
+    public static final void ebugERROR(String prefix,String text) {
+        if (enabled && level <= ERROR)
+        {
+            System.out.println("ERR: " + prefix + " " + text);
+        }
     }
 
 }
--- src/main/java/soc/disableDebug/D.java
+++ src/main/java/soc/disableDebug/D.java
@@ -2,6 +2,7 @@
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
  * Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
  * Portions of this file Copyright (C) 2007-2009,2014,2020 Jeremy D Monin <jeremy@nand.net>
+ * Portions of this file Copyright (C) 2017-2018 Strategic Conversation (STAC Project) https://www.irit.fr/STAC/
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,11 +25,50 @@ package soc.disableDebug;
  * Debug output; the disabled class is always off.
  * {@link soc.debug.D} and soc.disableDebug.D have the same interface, to easily switch
  * debug on and off per class.
+ *
+ * Extended with 4 levels of importance: {@link #INFO}, {@link #WARNING}, {@link #ERROR}, {@link #FATAL};
+ * Depending on the level, call one of the debug methods to print out.
  */
 public class D
 {
+    /**
+     * Print out everything
+     */
+    public static final int INFO = 0;
+    /**
+     * Print out warnings or above
+     */
+    public static final int WARNING = 1;
+    /**
+     * Print out errors or fatals
+     */
+    public static final int ERROR = 2;
+    /**
+     * Print out fatals only. NOTE: despite the name, fatals are exceptions that may or may not cause the application to crash
+     */
+    public static final int FATAL = 3;
+
     static public final boolean ebugOn = false;
     // static private boolean enabled = false;
+
+    /**
+     * The debug level one of: {@link #INFO}, {@link #WARNING}, {@link #ERROR}, {@link #FATAL}
+     * Default set to WARNING.
+     * Doesn't affect anything as debug is off
+     */
+    static private int level = WARNING;
+
+    /**
+     * Changes the debug level to one of: {@link #INFO}, {@link #WARNING}, {@link #ERROR}, {@link #FATAL}
+     * The default is WARNING.
+     */
+    public static void setLevel(int l){
+        level = l;
+    }
+
+    public static int ebug_level(){
+        return level;
+    }
 
     /**
      * Does nothing, since this is the disabled version.
@@ -42,6 +82,7 @@ public class D
 
     /**
      * Is debug currently enabled?
+     * Always returns false as debug is off
      * @since 1.1.00
      */
     public static final boolean ebugIsEnabled()
@@ -61,6 +102,25 @@ public class D
      */
     public static final void ebugPrintln() {}
 
+   /**
+     * Does nothing as debug is off
+     *
+     * @param text DOCUMENT ME!
+     */
+    public static final void ebugPrintlnINFO(String text) {}
+
+    /**
+     * Does nothing as debug is off
+     *
+     * @param text DOCUMENT ME!
+     */
+    public static final void ebugPrintlnINFO(String prefix, String text) {}
+
+    /**
+     * Does nothing as debug is off
+     */
+    public static final void ebugPrintlnINFO() {}
+
     /**
      * If debug is enabled, print the stack trace of this exception
      * @param ex Exception or other Throwable
@@ -71,6 +131,14 @@ public class D
     public static final void ebugPrintStackTrace(Throwable ex, String prefixMsg) {}
 
     /**
+     * Does nothing as debug is off
+     * @param ex Exception or other Throwable
+     * @param prefixMsg Message for {@link #ebugPrintlnINFO(String)} above the exception,
+     *                  or null
+     */
+    public static final void ebugFATAL(Throwable ex, String prefixMsg) {}
+
+    /**
      * DOCUMENT ME!
      *
      * @param text DOCUMENT ME!
@@ -78,10 +146,41 @@ public class D
     public static final void ebugPrint(String text) {}
 
     /**
+     * DOCUMENT ME!
+     *
+     * @param text DOCUMENT ME!
+     */
+    public static final void ebugPrintINFO(String text) {}
+
+    /**
      * Debug-println this text; for compatibility with log4j.
      * Calls {@link #ebugPrintln(String)}.
      * @param text Text to debug-print
      */
     public final void debug(String text) { ebugPrintln(text); }
+
+    /**
+     * Does nothing as debug is off
+     * @param text Text to debug-print
+     */
+    public static final void ebugWARNING(String text) { }
+
+    /**
+     * Does nothing as debug is off
+     * @param text Text to debug-print
+     */
+    public static final void ebugWARNING(String prefix, String text) { }
+
+    /**
+     * Does nothing as debug is off
+     * @param text Text to debug-print
+     */
+    public static final void ebugERROR(String text) { }
+
+    /**
+     * Does nothing as debug is off
+     * @param text Text to debug-print
+     */
+    public static final void ebugERROR(String prefix, String text) { }
 
 }
