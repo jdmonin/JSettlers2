5dd719ccb8806657c10d67c6dca461a7aa5b148c
Jeremy D Monin <jeremy@nand.net>
2020-09-27 08:54:15 -0400

    SOCBuildPlan upstreaming followup: Add getTotalResourcesForBuildPlan to interface from Stack; fix totalResourcesForBuidPlan typo and getResourcesToBuild call; javadocs, standard GPLv3 header and copyright attribs, serialVersionUID, etc

--- src/main/java/soc/robot/SOCBuildPlan.java
+++ src/main/java/soc/robot/SOCBuildPlan.java
@@ -1,9 +1,34 @@
+/**
+ * Java Settlers - An online multiplayer version of the game Settlers of Catan
+ * This file copyright (C) 2017-2018 Strategic Conversation (STAC Project) https://www.irit.fr/STAC/
+ * Portions of this file copyright (C) 2020 Jeremy D Monin <jeremy@nand.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The maintainer of this program can be reached at jsettlers@nand.net
+ **/
 package soc.robot;
 
+import soc.game.SOCResourceSet;
+
 /**
- * Interface for building plans, to be implemented as desired.
- * @author kho30
+ * Encapsulation for building plans. Implemented as {@link SOCBuildPlanStack} or otherwise.
+ *<P>
+ * Before v2.4.10, Building Plans were represented as {@code Stack<SOCPossiblePiece>}.
  *
+ * @author kho30
+ * @since 2.4.10
  */
 public interface SOCBuildPlan
 {
@@ -13,31 +38,33 @@ public interface SOCBuildPlan
     public void clear();
 
     /**
-     * Return true if nothing is currently planned
-     * @return
+     * Is this plan currently empty?
+     * @return true if nothing is currently planned
      */
     public boolean isEmpty();
 
     /**
-     * Get the ith planned build item.
+     * Get the <em>i</em>th planned build item.
      * This is typically called with an index of 0 (the first piece, equivalent to a peek),
      * however it is called with an index of 1 during the play of a Road Building card.
-     *
+     *<P>
      * Note: This may be unsafe - assumes an appropriate size of build plan.  Could easily add a check for size in the
-     * function call, but I think it's probably easier to debug for now with bad calls throwing exceptions, rather than
+     * function call, but it's probably easier to debug for now with bad calls throwing exceptions, rather than
      * returning nulls that may be harder to figure out if they create funny behavior later.
-     *
-     * Non-linear build plans should be discussed as to how to implement this.  I'm imagining we would
+     *<P>
+     * In future, non-linear build plans should be discussed as to how to implement this. For example if we
      * traverse a tree-like structure, so separate functions would need to be added to switch between branches.
      *
-     * @param pieceNum
-     * @return
+     * @param pieceNum  Piece number within plan, where 0 is the first to be built.
+     *     Range is 0 to {@link #getPlanDepth()} - 1.
+     * @return  Piece within plan
      */
     public SOCPossiblePiece getPlannedPiece(int pieceNum);
 
     /**
-     * Return the depth of the plan.  Non-linear plans to be discussed.
-     * @return
+     * Return the depth of the plan: The number of pieces to be built. Non-linear plans to be discussed in future.
+     * @return Number of pieces in this plan
+     * @see #getPlannedPiece(int)
      */
     public int getPlanDepth();
 
@@ -45,5 +72,11 @@ public interface SOCBuildPlan
      * Step forward in the plan.  Equivalent to a pop in the stack implementation.
      */
     public void advancePlan();
+
+    /**
+     * Calculate the total resources needed to build all pieces in this plan.
+     * @return Total resources, from each piece's {@link SOCPossiblePiece#getResourcesToBuild()}
+     */
+    public SOCResourceSet getTotalResourcesForBuildPlan();
 
 }
--- src/main/java/soc/robot/SOCBuildPlanStack.java
+++ src/main/java/soc/robot/SOCBuildPlanStack.java
@@ -1,3 +1,23 @@
+/**
+ * Java Settlers - An online multiplayer version of the game Settlers of Catan
+ * This file copyright (C) 2017-2018 Strategic Conversation (STAC Project) https://www.irit.fr/STAC/
+ * Portions of this file copyright (C) 2020 Jeremy D Monin <jeremy@nand.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The maintainer of this program can be reached at jsettlers@nand.net
+ **/
 package soc.robot;
 
 import java.io.Serializable;
@@ -6,16 +26,19 @@ import java.util.Stack;
 import soc.game.SOCResourceSet;
 
 /**
- * Stack implementation of a Build Plan.
- *
+ * Stack implementation of a {@link SOCBuildPlan}.
+ *<P>
  * Uses stack directly to save time - will need to change
  * to a wrapper if we ever change SOCBuildPlan to an abstract class instead of an interface.
  *
  * @author kho30
+ * @since 2.4.10
  */
 public class SOCBuildPlanStack extends Stack<SOCPossiblePiece>
     implements SOCBuildPlan, Serializable
 {
+    /** Last structural change was v2.4.10 */
+    private static final long serialVersionUID = 2410L;
 
     /**
      * NB: This does not check for a legal index
@@ -38,19 +61,14 @@ public class SOCBuildPlanStack extends Stack<SOCPossiblePiece>
         pop();
     }
 
-    /**
-     * Return the total resources needed for building all the pieces on the stack.
-     * @return 
-     */
-    public SOCResourceSet totalResourcesForBuidPlan()
+    public SOCResourceSet getTotalResourcesForBuildPlan()
     {
         SOCResourceSet rs = new SOCResourceSet();
 
-        for (Enumeration e = super.elements(); e.hasMoreElements(); )
+        for (Enumeration<SOCPossiblePiece> e = super.elements(); e.hasMoreElements(); )
         {
             SOCPossiblePiece pp = (SOCPossiblePiece) e.nextElement();
-            SOCResourceSet addedResources = pp.getResourceCost();
-            rs.add(addedResources);
+            rs.add(pp.getResourcesToBuild());
         }
 
         return rs;
