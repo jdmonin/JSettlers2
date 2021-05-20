jdmonin/JSettlers2

commit 12638f3abfc8ca1511bdcd2bdc373d36fcc99065

SOCBoardPanel perf, based on issue #84 discussion with tiehfood: Use BufferedImage, VolatileImage; +loadBufferedImage; drawBoard, paintComponent call image.flush less often

src/main/resources/resources/version.info
@@ -7,7 +7,7 @@ project.versionnum=2450
 # should be a version released more than a year ago.
 project.versionnumMaxNoWarn=2000
 project.copyright=2001-2004 Robb Thomas, portions (C) 2007-2021 Jeremy D Monin and contributors
-project.buildnum=JM20210119
+project.buildnum=JM20210124
 project.jre.min.major=1
 project.jre.min.minor=7
 project.jre.min.edit=0

src/main/java/soc/client/SwingMainDisplay.java
@@ -1,6 +1,6 @@
 /**
  * Java Settlers - An online multiplayer version of the game Settlers of Catan
- * This file copyright (C) 2019-2020 Jeremy D Monin <jeremy@nand.net>
+ * This file copyright (C) 2019-2021 Jeremy D Monin <jeremy@nand.net>
  * Extracted in 2019 from SOCPlayerClient.java, so:
  * Portions of this file Copyright (C) 2003  Robert S. Thomas <thomas@infolab.northwestern.edu>
  * Portions of this file copyright (C) 2007-2019 Jeremy D Monin <jeremy@nand.net>
@@ -106,6 +106,8 @@ import soc.util.Version;
  *</UL>
  * Individual games are shown using {@link SOCPlayerInterface}
  * and channels use {@link ChannelFrame}.
+ *<P>
+ * Should be added directly to a {@link JFrame} or other {@link Frame}, not a subcontainer.
  *<P>
  * Before v2.0.00, most of these fields and methods were part of the main {@link SOCPlayerClient} class.
  * Also converted from AWT to Swing in v2.0.00.

commit da7915df14aeee706c12c037d1a0e114af38cab8

SOCBoardPanel.drawSuperText: To fix box width bug caused by 12638f3/issue #84, specify font like in drawSuperTextTop; previous release 2.4.00 doesn't have this bug

src/main/java/soc/client/SOCBoardPanel.java
@@ -5404,18 +5404,15 @@ import javax.swing.JComponent;
      */
     private void drawSuperText(Graphics g)
     {
+        // Specify the font, so we know its metrics.
+        // This avoids a timing bug where the wrong font's metrics are used.
+        final Font bpf = new Font("Dialog", Font.PLAIN, 10 * playerInterface.displayScale);
+
         // Do we need to calculate the metrics?
 
         if ((superText1_w == 0) || ((superText2 != null) && (superText2_w == 0)))
         {
-            final Font bpf = getFont();
-            if (bpf == null)
-            {
-                repaint();  // We'll have to try again
-                return;
-            }
-
-            final FontMetrics fm = getFontMetrics(bpf);
+            final FontMetrics fm = g.getFontMetrics(bpf);
             if (fm == null)
             {
                 repaint();
@@ -5464,6 +5461,9 @@ import javax.swing.JComponent;
         int ty = superTextBox_y + SUPERTEXT_INSET + superText_h - superText_des;
         if (superText1 == null)
             return;  // avoid NPE from multi-threading
+
+        final Font prev = g.getFont();
+        g.setFont(bpf);
         g.drawString(superText1, tx, ty);
         if (superText2 != null)
         {
@@ -5471,6 +5471,7 @@ import javax.swing.JComponent;
             ty += superText_h;
             g.drawString(superText2, tx, ty);
         }
+        g.setFont(prev);
     }
 
     /**
@@ -5479,8 +5480,8 @@ import javax.swing.JComponent;
      */
     private void drawSuperTextTop(Graphics g)
     {
-        // Force the font, so we know its metrics.
-        // This avoids an OSX fm.stringWidth bug.
+        // Specify the font, so we know its metrics.
+        // This avoids a timing bug where the wrong font's metrics are used.
         final Font bpf = new Font("Dialog", Font.PLAIN, 10 * playerInterface.displayScale);
 
         // Do we need to calculate the metrics?
@@ -7251,7 +7252,8 @@ import javax.swing.JComponent;
     {
         if ((superText1 == text1) && (superText2 == text2))
         {
-            return;  // <--- Early return: text unchanged ---
+            return;  // <--- Early return: text obviously unchanged ---
+
             // This quick check is an optimization.
             // Any of the 4 variables could be null.
             // It's not worth the additional complexity

src/main/resources/resources/version.info
@@ -7,7 +7,7 @@ project.versionnum=2450
 # should be a version released more than a year ago.
 project.versionnumMaxNoWarn=2000
 project.copyright=2001-2004 Robb Thomas, portions (C) 2007-2021 Jeremy D Monin and contributors
-project.buildnum=JM20210518
+project.buildnum=JM20210519
 project.jre.min.major=1
 project.jre.min.minor=7
 project.jre.min.edit=0