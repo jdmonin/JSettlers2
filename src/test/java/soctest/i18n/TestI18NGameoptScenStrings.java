1b7a5aee07e35dcb016a861da9df87e50dab338a
Jeremy D Monin <jeremy@nand.net>
2026-04-29 07:47:08 -0400

    TestI18NGameoptScenStrings.testDescriptionsForNet: Also compare a few strings server/client for consistency; synchronize common phrasing for 2 PL localized strings

--- src/test/java/soctest/i18n/TestI18NGameoptScenStrings.java
+++ src/test/java/soctest/i18n/TestI18NGameoptScenStrings.java
@@ -218,25 +218,53 @@ public class TestI18NGameoptScenStrings
     }
 
     /**
-     * For {@link #testDescriptionsForNet()}, test one string props file's description strings.
-     * @param pfile Full filename to open and test
+     * For {@link #testDescriptionsForNet()}, test one pair of string props files' description strings.
+     * @param pfile  Server strings file to open and test
+     * @param pfileCli  Client strings file to open and test
+     * @param localeSuffix  Filename suffix for locale being tested, like {@code "_es.properties"}
      * @return True if OK, or prints failed strings and return false
-     * @throws Exception if {@code pfile} can't be opened and read
+     * @throws Exception if {@code pfile} or {@code pfileCli} can't be opened and read
      */
-    private boolean testDescriptionsFile(File pfile)
+    private boolean testDescriptionsFiles
+        (File pfile, File pfileCli, final String localeSuffix)
         throws Exception
     {
-        final FileInputStream fis = new FileInputStream(pfile);
-        final PropertyResourceBundle props = new PropertyResourceBundle(fis);
+        FileInputStream fis = new FileInputStream(pfile);
+        final PropertyResourceBundle propsSrv = new PropertyResourceBundle(fis);
+        try { fis.close(); }
+        catch (IOException e) {}
+
+        fis = new FileInputStream(pfileCli);
+        final PropertyResourceBundle propsCli = new PropertyResourceBundle(fis);
         try { fis.close(); }
         catch (IOException e) {}
 
         boolean allOK = true;
 
         final TreeSet<String> optBadChar  = new TreeSet<>(), // use TreeSet for sorted results
-                              scenBadChar = new TreeSet<>();
+                              scenBadChar = new TreeSet<>(),
+                              optDescMissingScenPrefix = new TreeSet<>();
         final ArrayList<String> optsStr = new ArrayList<>(),
-                                scenStr = new ArrayList<>();
+                                scenStr = new ArrayList<>(),
+                                keyNotSameAtSrvCli = new ArrayList<>();
+
+        // Client NGOF hides a redundant "Scenarios: " consistent prefix in scenario game option descriptions,
+        // but the gameopt text is stored at server, so we'll compare server opt strings against that prefix from client strings.
+        String gameoptScenDescPrefix = null;
+        final boolean gameoptScenDescMissingOK = (localeSuffix.startsWith("_pl."));
+        try
+        {
+            gameoptScenDescPrefix = propsCli.getString("game.options.scenario.optprefix");
+            assertNotNull(gameoptScenDescPrefix);
+            assertFalse(gameoptScenDescPrefix.trim().isEmpty());
+        } catch (MissingResourceException e) {
+            // unlikely to be missing, except in deliberately-empty _en file
+            if (! localeSuffix.startsWith("_en."))
+            {
+                fail(pfileCli.getName() + ": Missing required key game.options.scenario.optprefix");
+                return false;  // to satisfy compiler; fail will end execution
+            }
+        }
 
         for (final SOCGameOption opt : allOpts)
         {
@@ -244,7 +272,8 @@ public class TestI18NGameoptScenStrings
             // but if present there the description strings do need to be OK.
             try
             {
-                final String smDesc = props.getString("gameopt." + opt.key);
+                final String key = "gameopt." + opt.key;
+                final String smDesc = propsSrv.getString(key);
                 if (smDesc != null)
                 {
                     optsStr.add(smDesc);
@@ -252,6 +281,10 @@ public class TestI18NGameoptScenStrings
                         checkDescForSortRank(opt, smDesc);
                     else
                         optBadChar.add(opt.key);
+
+                    if (opt.key.startsWith("_SC_") && (! gameoptScenDescMissingOK)
+                        && (! smDesc.startsWith(gameoptScenDescPrefix)))
+                        optDescMissingScenPrefix.add(opt.key);
                 }
             } catch (MissingResourceException e) {}
         }
@@ -261,7 +294,7 @@ public class TestI18NGameoptScenStrings
             String strKey = sc.key + ".n";
             try
             {
-                final String smDesc = props.getString("gamescen." + strKey);
+                final String smDesc = propsSrv.getString("gamescen." + strKey);
                 if (smDesc != null)
                 {
                     scenStr.add(smDesc);
@@ -278,7 +311,7 @@ public class TestI18NGameoptScenStrings
                 strKey = sc.key + ".d";
                 try
                 {
-                    final String smDesc = props.getString("gamescen." + strKey);
+                    final String smDesc = propsSrv.getString("gamescen." + strKey);
                     if (smDesc != null)
                     {
                         scenStr.add(smDesc);
@@ -286,6 +319,34 @@ public class TestI18NGameoptScenStrings
                             scenBadChar.add(strKey);
                     }
                 } catch (MissingResourceException e) {}
+            }
+        }
+
+        // Check for a few strings which should be the same at client and server
+        for (String key : new String[]{
+            "action.rolled.sc_piri.player.lost.rsrcs.to.fleet", "action.rolled.sc_piri.player.tied",
+            "action.rolled.sc_piri.player.won.pick.free", "action.rolled.sc_piri.you.lost.rsrcs.to.fleet"
+        })
+        {
+            String srvVal = null, cliVal = null;
+            try
+            {
+                srvVal = propsSrv.getString(key);
+            } catch (MissingResourceException e) {}
+            try
+            {
+                cliVal = propsCli.getString(key);
+            } catch (MissingResourceException e) {}
+
+            if (srvVal == null)
+            {
+                if (cliVal != null)
+                    keyNotSameAtSrvCli.add(key + ": missing in " + pfile.getName());
+            } else {
+                if (cliVal == null)
+                    keyNotSameAtSrvCli.add(key + ": missing in " + pfileCli.getName());
+                else if (! srvVal.equals(cliVal))
+                    keyNotSameAtSrvCli.add(key + ": text differs in " + pfile.getName() + " vs " + pfileCli.getName());
             }
         }
 
@@ -303,6 +364,21 @@ public class TestI18NGameoptScenStrings
             System.out.println
                 (pfile.getName() + ": SOCScenario key strings in gamescen.* failing SOCMessage.isSingleLineAndSafe(..): "
                  + scenBadChar);
+        }
+
+        if (! optDescMissingScenPrefix.isEmpty())
+        {
+            allOK = false;
+            System.out.println
+                (pfile.getName() + ": Scenario game opt strings in gameopt.* missing prefix text from client key game.options.scenario.optprefix: "
+                 + optDescMissingScenPrefix);
+        }
+
+        if (! keyNotSameAtSrvCli.isEmpty())
+        {
+            allOK = false;
+            System.out.println
+                ("these keys should be same at server, client: " + keyNotSameAtSrvCli);
         }
 
         final int MAX = Connection.MAX_MESSAGE_SIZE_UTF8;  // alias for brevity
@@ -337,6 +413,13 @@ public class TestI18NGameoptScenStrings
      *<P>
      * If a locale's strings fail versus {@link Connection#MAX_MESSAGE_SIZE_UTF8}, the server code should
      * be enhanced to break them into multiple messages when that situation arises.
+     *<P>
+     * Also checks:
+     *<UL>
+     * <LI> Each server locale file has a corresponding client locale file
+     * <LI> {@code action.rolled.sc_piri.*} text is the same at server and client
+     * <LI> Server's scenario-related game option descriptions begin with text of client key {@code "game.options.scenario.optprefix"}
+     *</UL>
      *
      * @throws Exception if any locale's props file can't be opened and read
      */
@@ -344,30 +427,53 @@ public class TestI18NGameoptScenStrings
     public void testDescriptionsForNet()
         throws Exception
     {
-        String pfull = SOCStringManager.PROPS_PATH_SERVER_FOR_CLIENT + ".properties";
-        if (pfull.charAt(0) != '/')
-            pfull = '/' + pfull;  // we're in a separate package from SOCStringManager
-        final URL u = SOCStringManager.class.getResource(pfull);
-        assertNotNull("Couldn't find " + pfull, u);
+        String pfullSrv = SOCStringManager.PROPS_PATH_SERVER_FOR_CLIENT + ".properties";
+        if (pfullSrv.charAt(0) != '/')
+            pfullSrv = '/' + pfullSrv;  // we're in a separate package from SOCStringManager
+        final URL uSrv = SOCStringManager.class.getResource(pfullSrv);
+        assertNotNull("Couldn't find " + pfullSrv, uSrv);
 
-        final File uf = new File(u.getPath());
-        final File dir = new File(uf.getParent());
-        assertTrue("Dir for " + pfull + " should exist", dir.isDirectory());
-        String pname = uf.getName();
-        assertTrue(pname.endsWith(".properties"));
-        pname = pname.substring(0, pname.length() - ".properties".length());  // to use as prefix in loop
+        String pfullCli = SOCStringManager.PROPS_PATH_AT_CLIENT + ".properties";
+        if (pfullCli.charAt(0) != '/')
+            pfullCli = '/' + pfullCli;  // we're in a separate package from SOCStringManager
+        final URL uCli = SOCStringManager.class.getResource(pfullCli);
+        assertNotNull("Couldn't find " + pfullCli, uSrv);
+
+        final File ufSrv = new File(uSrv.getPath());
+        final File dirSrv = new File(ufSrv.getParent());
+        assertTrue("Dir for server strings " + pfullSrv + " should exist", dirSrv.isDirectory());
+
+        final File ufCli = new File(uCli.getPath());
+        final File dirCli = new File(ufCli.getParent());
+        assertTrue("Dir for client strings " + pfullCli + " should exist", dirCli.isDirectory());
+
+        String pnameSrv = ufSrv.getName();
+        assertTrue(pnameSrv.endsWith(".properties"));
+        pnameSrv = pnameSrv.substring(0, pnameSrv.length() - ".properties".length());  // to use as prefix in loop
+        final int pnameSrvLen = pnameSrv.length();
+
+        String pnameCli = ufCli.getName();
+        assertTrue(pnameCli.endsWith(".properties"));
+        pnameCli = pnameCli.substring(0, pnameCli.length() - ".properties".length());  // to get client file based on server filename
 
         boolean allOK = true;
-        for (final File f : dir.listFiles())
+        for (final File fSrv : dirSrv.listFiles())
         {
-            if (! f.getName().startsWith(pname))
+            final String fnameSrv = fSrv.getName();
+            if (! fnameSrv.startsWith(pnameSrv))
                 continue;
-            if (! testDescriptionsFile(f))
+
+            String localeSuffix = fnameSrv.substring(pnameSrvLen);
+            String fnameCli = pnameCli + localeSuffix;
+            File fCli = new File(dirCli, fnameCli);
+            assertTrue("Client file exists: " + fnameCli + " for server " + fnameSrv, fCli.exists());
+
+            if (! testDescriptionsFiles(fSrv, fCli, localeSuffix))
                 allOK = false;
         }
 
         assertTrue
-            ("All locales' gameopt and scenario descs should have no unsendable characters: " + pfull, allOK);
+            ("Localized gameopt and scenario strings; see test output for details: " + pfullSrv, allOK);
     }
 
     public static void main(String[] args)
